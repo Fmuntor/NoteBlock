237f30a11687d85a6f572c6973b78864394ffbd2
Fernando <damdaw209f@gmail.com>
2023-10-26 18:44:19 +0200

    1/8 Area de texto con barra de scroll

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,42 @@
-public class App {
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class GUI {
+
+    JFrame window;
+    // Crear area de texto
+    JTextArea textArea;
+    // Crear scroll lateral
+    JScrollPane scrollPane;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        new GUI();
+    }
+
+    public GUI() {
+        createWindow();
+        createTextArea();
+        window.setVisible(true);
+
+    }
+
+    public void createWindow(){
+        window = new JFrame("NoteBlock");
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void createTextArea(){
+        textArea = new JTextArea();
+
+        scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+        //Eliminar el borde al scroll
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        window.add(scrollPane);
+
+        //window.add(textArea);
     }
 }
