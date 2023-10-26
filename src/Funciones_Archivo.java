d9a0439fb0f399ee6b8f62634cf02d62278382f5
Fernando <damdaw209f@gmail.com>
2023-10-26 19:29:04 +0200

    4/8 Leer archivos de texto

--- src/Funciones_Archivo.java
+++ src/Funciones_Archivo.java
@@ -1,18 +1,47 @@
 import java.awt.FileDialog;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Funciones_Archivo {
-    
-    GUI gui;
 
-    public Funciones_Archivo(GUI gui){
+    GUI gui;
+    String nombreArchivo;
+    String direccionArchivo;
+
+    public Funciones_Archivo(GUI gui) {
         this.gui = gui;
     }
-    public void nuevo(){
+
+    public void nuevo() {
         gui.textArea.setText("");
         gui.window.setTitle("Nuevo");
     }
-    public void abrir(){
+
+    public void abrir() {
         FileDialog fd = new FileDialog(gui.window, "Abrir", FileDialog.LOAD);
         fd.setVisible(true);
+        if (fd.getFile() != null) {
+            // Leer el nombre y direccion del archivo seleccionado
+            nombreArchivo = fd.getFile();
+            direccionArchivo = fd.getDirectory();
+            // Asignar a la ventana el nombre del archivo seleccionado
+            gui.window.setTitle(nombreArchivo);
+        }
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(direccionArchivo + nombreArchivo)); // Hay que añadir el nombre a la ruta
+
+            gui.textArea.setText("");
+
+            String linea = null;
+
+            // Este BufferedReader lee el texto linea a linea
+            while((linea= br.readLine())!=null){
+                gui.textArea.append(linea + "\n");
+            }
+            br.close();
+
+        } catch(Exception e){
+            System.out.println("ARCHIVO NO ABIERTO");
+        }
     }
 }
