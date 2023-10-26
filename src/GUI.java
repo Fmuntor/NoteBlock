c48f188b42ffc7c8fbea51549f1be2c0693c8442
Fernando <damdaw209f@gmail.com>
2023-10-26 19:00:19 +0200

    2/8 Barra superior y Menu Items

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,8 @@
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -10,6 +13,12 @@ public class GUI {
     JTextArea textArea;
     // Crear scroll lateral
     JScrollPane scrollPane;
+    // Crear barra de menu
+    JMenuBar menuBar;
+    // Crear items de barra de menu
+    JMenu menuArchivo, menuEditar, menuFormato, menuColor;
+    // Crear items de la pestaña Archivo
+    JMenuItem iNuevo, iAbrir, iGuardar, iGuardarComo, iSalir;//itemCortar, itemPegar
 
     public static void main(String[] args) throws Exception {
         new GUI();
@@ -18,6 +27,8 @@ public class GUI {
     public GUI() {
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
         window.setVisible(true);
 
     }
@@ -38,5 +49,41 @@ public class GUI {
         window.add(scrollPane);
 
         //window.add(textArea);
+    }
+
+    public void createMenuBar(){
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuArchivo = new JMenu("Archivo");
+        menuBar.add(menuArchivo);
+
+        menuEditar = new JMenu("Editar");
+        menuBar.add(menuEditar);
+
+        menuFormato = new JMenu("Formato");
+        menuBar.add(menuFormato);
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+    }
+
+    public void createFileMenu(){
+
+        iNuevo = new JMenuItem("Nuevo");
+        menuArchivo.add(iNuevo);
+
+        iAbrir = new JMenuItem("Abrir");
+        menuArchivo.add(iAbrir);
+
+        iGuardar = new JMenuItem("Guardar");
+        menuArchivo.add(iGuardar);
+
+        iGuardarComo = new JMenuItem("Guardar como");
+        menuArchivo.add(iGuardarComo);
+
+        iSalir = new JMenuItem("Salir");
+        menuArchivo.add(iSalir);
+
     }
 }
